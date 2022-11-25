7a6f66b0139895f72e4908601bd6def3215bc347
jtvandijk <j.t.vandijk@ucl.ac.uk>
2022-11-25 17:22:37 +0000

    update dirty fix for index.html next page

--- _set_part.py
+++ _set_part.py
@@ -20,13 +20,17 @@ for htmlf in html_files:
     with open(htmlf) as file :
       html_content = file.read()
 
-    # update
+    # update TOC
     html_content = html_content.replace('<li class="chapter" data-level="" data-path="index.html"><a href="index.html"><i class="fa fa-check"></i>Module Overview</a></li>', \
                                         '<li class="part"><span><b>Module Overview</b></span></li>')
     html_content = html_content.replace('<li class="chapter" data-level="" data-path="foundational-concepts.html"><a href="foundational-concepts.html"><i class="fa fa-check"></i>Foundational Concepts</a></li>', \
                                         '<li class="part"><span><b>Foundational Concepts</b></span></li>')
     html_content = html_content.replace('<li class="chapter" data-level="" data-path="additional-resources.html"><a href="additional-resources.html"><i class="fa fa-check"></i>Additional Resources</a></li>', \
                                         '<li class="part"><span><b>Additional Resources</b></span></li>')
+
+    # update back and forth buttons
+    html_content = html_content.replace('<link rel="next" href="module-introduction.html"/>', \
+                                        '<link rel="next" href="geocomputation-an-introduction.html"/>')
 
     # write
     with open(htmlf, 'w') as file:
